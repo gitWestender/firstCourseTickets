d38dd58c2da3fd80a9dee17b62f6a8feccf02c21
Westender <javaxwestender@gmail.com>
2023-07-23 15:44:02 +0300

    Add 1st method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4};
+
+        reverseArray(array);
 
     }
+
+    static int[] reverseArray(int[] array) {
+        int tmp;
+
+        for (int i = 0; i < array.length / 2; i++) {
+            tmp = array[i];
+            array[i] = array[array.length - i - 1];
+            array[array.length - i - 1] = tmp;
+        }
+        return array;
+    }
+
+
 }
